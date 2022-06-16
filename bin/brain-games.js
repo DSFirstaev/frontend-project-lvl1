1c5d73877fd798495076cca8c6aa46863cccc8c5
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-06-16 21:17:15 +0300

    add greeting User

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 //При запуске исполняемого файла такая запись позволит выполнить код, который в нём расположен
 
+import { greetUser }  from './../src/cli.js';
+
 // выводит приветственное сообщение
 console.log('Welcome to the Brain Games!');
 
+greetUser();
