7efa24f196f2e6c0c0a970276caf983b701d7474
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-06-17 20:33:11 +0300

    fix issues by linter

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
-import  readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-export const greetUser = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+const greetUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
+export default greetUser;
