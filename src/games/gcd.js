964b5ace020ab3236ace4b1a9c58f9a11d1062a2
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-06-25 19:37:05 +0300

    fix bug

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,7 +5,7 @@ const rules = 'Find the greatest common divisor of given numbers.';
 const findGCD = (numbers) => {
   const result = [];
   const sortNumbers = numbers.sort();
-  for (let i = 1; i < sortNumbers[0] / 2; i += 1) {
+  for (let i = 1; i <= sortNumbers[0]; i += 1) {
     if (sortNumbers[0] % i === 0 && sortNumbers[1] % i === 0) {
       result.push(i);
     }
