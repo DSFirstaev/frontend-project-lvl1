e48c311f0d4000c5438f9d818eabf921f2f19be2
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-06-26 14:19:10 +0300

    add random length of progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,15 +6,15 @@ const progression = () => {
   const startNum = getRandomNumber(0, 20);
   const progNum = getRandomNumber(1, 10);
   const result = [];
-  const countNum = 10;
-  for (let i = 0; i < countNum; i += 1) {
+  const lengthOfProgression = getRandomNumber(5, 10);
+  for (let i = 0; i <= lengthOfProgression; i += 1) {
     result.push(startNum + i * progNum);
   }
-  const randomIndex = getRandomNumber(0, countNum - 1);
+  const randomIndex = getRandomNumber(0, lengthOfProgression - 1);
   const dots = '..';
   const rightAnswer = String(result[randomIndex]);
   result[randomIndex] = dots;
-  const task = `Question: ${result[0]} ${result[1]} ${result[2]} ${result[3]} ${result[4]} ${result[5]} ${result[6]} ${result[7]} ${result[8]} ${result[9]}`;
+  const task = `Question: ${result.join([' '])}`;
   return [rightAnswer, task];
 };
 
